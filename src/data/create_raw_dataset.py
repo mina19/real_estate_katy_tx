efcd99ed4beac59a2d0649b2e58dfaa977f49bb2
MCZeno <MCZeno@eprod.com>
2024-03-15 14:43:44 +0000

    Update functions in create_raw_dataset

--- src/data/create_raw_dataset.py
+++ src/data/create_raw_dataset.py
@@ -1,9 +1,21 @@
-import pandas as pd
-import numpy as np
 import glob
 
-def raw_filenames(rel_dir: str="../data/raw/*") -> list:
+import pandas as pd
+
+
+def raw_filenames(rel_dir: str = "../data/raw/*") -> list:
     """Returns the raw data filenames in a list"""
     raw_data_filenames = glob.glob(rel_dir)
-    raw_data_filenames = [file for file in raw_data_filenames if file.endswith('.csv')]
+    raw_data_filenames = [file for file in raw_data_filenames if file.endswith(".csv")]
     return raw_data_filenames
+
+
+def raw_data(raw_data_filenames: list) -> list:
+    """Returns the raw data in a dataframe"""
+    raw_df = pd.DataFrame()
+    for file in raw_data_filenames:
+        df = pd.read_csv(file)
+        mask = df["SALE TYPE"] == "PAST SALE"
+        raw_df = pd.concat([raw_df, df[mask]])
+
+    return raw_df
