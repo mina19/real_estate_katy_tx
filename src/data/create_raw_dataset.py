104f4312006163c9be1b06f22867c037c6ed70ce
MCZeno <MCZeno@eprod.com>
2024-03-15 17:07:28 +0000

    Save work

--- src/data/create_raw_dataset.py
+++ src/data/create_raw_dataset.py
@@ -6,6 +6,8 @@ import pandas as pd
 def raw_filenames(rel_dir: str = "../data/raw/*") -> list:
     """Returns the raw data filenames in a list"""
     raw_data_filenames = glob.glob(rel_dir)
+
+    # Ignore the README.md file
     raw_data_filenames = [file for file in raw_data_filenames if file.endswith(".csv")]
     return raw_data_filenames
 
@@ -15,6 +17,10 @@ def raw_data(raw_data_filenames: list) -> pd.DataFrame:
     raw_df = pd.DataFrame()
     for file in raw_data_filenames:
         df = pd.read_csv(file)
+
+        # Remove header line that says
+        # 'In accordance with local MLS rules, some MLS listings are not included in the download'
+        # in the SALE TYPE column
         mask = df["SALE TYPE"] == "PAST SALE"
         raw_df = pd.concat([raw_df, df[mask]], ignore_index=True)
 
@@ -32,20 +38,23 @@ def prepare_df(df: pd.DataFrame) -> pd.DataFrame:
             "hoa/month": "hoa_per_month",
         }
     )
+
+    # Remove townhomes from analysis
     mask = df["property_type"] == "Single Family Residential"
     df = df[mask]
 
+    # Only interested in Katy
     mask = df["city"] == "Katy"
     df = df[mask]
 
     df = df.drop(
         [
-            "property_type",
-            "city",
-            "sale_type",
-            "days_on_market",
-            "state_or_province",
-            "sold_date",
+            "property_type",  # all the same
+            "city",  # all the same
+            "sale_type",  # all the same
+            "days_on_market",  # unavailable
+            "state_or_province",  # all the same
+            "sold_date",  # not important (may be cool to see how this is related to interested rates)
         ],
         axis=1,
     )
